1b3ea7b6b61083698a443009b896c77fcca03d24
charll <16559726+charlllleee@user.noreply.gitee.com>
2026-08-03 21:37:37 +0800

    3

---
+++ list/for_list.py
@@ -0,0 +1,23 @@
+animals=['dog','cat','horse','lion']
+for animal in animals:
+    print(f'I like {animal}!')
+print('\nI really love animals!')
+
+
+    
+numbers=(range(1,1000001))
+print(min(numbers))
+print(max(numbers))
+print(sum(numbers))
+
+list_1=(range(1,20,2))
+for list_2 in list_1:
+    print(list_2)
+
+num=(range(3,31,3))
+for num_1 in num:
+    print(num_1)
+
+lifang=[shu**3 for shu in range(1,11)]
+for num_2 in lifang:
+    print(num_2)
---
+++ list/slice.py
@@ -0,0 +1,3 @@
+players=['charles','martina','macheal','florence','eli']
+print('The first three items in the list are:')
+print(players[:3])
