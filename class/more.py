5b8c16c88533122ab813d83eec2299d840464480
charll <16559726+charlllleee@user.noreply.gitee.com>
2026-09-08 11:44:53 +0800

    9

---
+++ class/more.py
@@ -0,0 +1,30 @@
+class User:
+    def __init__(self,f_name,l_name,gender,school):
+        self.f_name=f_name
+        self.l_name=l_name
+        self.gender=gender
+        self.school=school
+        self.number=0
+
+    def describe_user(self):
+        print(f"{self.f_name}{self.l_name} {self.gender} {self.school}")
+
+    def greet_user(self):
+        print(f"Hello {self.f_name}{self.l_name},welcome come to here.")
+
+    def increment_login_attempts(self,new_user=1):
+        self.number += new_user
+
+    def reset_attempts(self,number=0):
+        self.number=number
+
+user1=User('wang','yongsheng','male','tianjingongyedaxue')
+user1.describe_user()
+user1.greet_user()
+user1.increment_login_attempts()
+user1.increment_login_attempts()
+user1.increment_login_attempts()
+user1.increment_login_attempts()
+print(user1.number)
+user1.reset_attempts()
+print(user1.number)
--- class/simple.py
+++ class/simple.py
@@ -1,7 +1,8 @@
 class Resteurant:
-    def __init__(self,name,type):
+    def __init__(self,name,type,):
         self.name=name
         self.type=type
+        self.number_save=0
 
     def describe_restaurant(self):
         print(f"{self.name} provide {self.type}.")
@@ -9,8 +10,25 @@ class Resteurant:
     def open_restaurant(self):
         print(f"The {self.name} is open.")
 
+    def set_number_saved(self,new_number_save):
+        if self.number_save<=new_number_save:
+            self.number_save=new_number_save
+        else:
+            print("Please enter anothor number.")
+
+    def increment_number_saved(self,increment_number):
+        self.number_save += increment_number
+
 restaurant=Resteurant('兰州牛肉面','noodles')
 print(restaurant.name)
 print(restaurant.type)
 restaurant.describe_restaurant()
 restaurant.open_restaurant()
+restaurant_1=Resteurant('古丽花儿','xingjiang foods')
+restaurant_1.describe_restaurant()
+restaurant.number_save=5
+print(restaurant.number_save)
+restaurant.set_number_saved(8)
+print(restaurant.number_save)
+restaurant.increment_number_saved(5)
+print(restaurant.number_save)
